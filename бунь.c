e057b3ecc0756b34238d2c56f5623bfbb755e286
lavruxxxa <70812017+lavruxxxa@users.noreply.github.com>
2020-09-16 13:43:14 +0300

    Update бунь.c

--- бунь.c
+++ бунь.c
@@ -1,22 +1,22 @@
 ﻿// лаба.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {   
     int ms[]= { 0,1,2,3,4,5,6,7,8,9 };
-    for (int i = 0; i < 10/2; i++)
+    for (int i = 0; i < 5; i++)
     {
         int a;
-            a = ms[10 - i];
-            ms[10 - i] = ms[i];
+            a = ms[9 - i];
+            ms[9 - i] = ms[i];
             ms[i] = a;
     }
     for (int i = 0; i < 10; i++)
     {
-        cout << ms[i] << " ";
+       printf(" %d ", ms[i]);
     }
+    system("pause");
     return 0;
 }
